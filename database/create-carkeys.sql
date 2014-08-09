31feaeaf85139ece6377c23dbe9b5a5d85ab81fb
Germán Bobr <german@germanbobr.com.ar>
2014-08-08 21:39:19 -0300

    Actualizado script sql para instalar carkeys
    El script create-carkeys.sql deberá ser ejecutado cuando se instale la
    nueva versión del gm en el server.
    Esto es para importar los viejos datos de propiedad de los vehiculos al
    nuevo esquema de llaves.

--- database/create-carkeys.sql
+++ database/create-carkeys.sql
@@ -30,10 +30,14 @@ CREATE TABLE IF NOT EXISTS `carkeys` (
   `keyId` int(11) NOT NULL AUTO_INCREMENT,
   `accountId` int(11) NOT NULL,
   `vehId` int(11) NOT NULL,
-  `isOwnKey` int(1) NOT NULL,
+  `ownerId` int(11) NOT NULL,
   PRIMARY KEY (`keyId`),
   KEY `accountId` (`accountId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+
+insert into carkeys(accountid,vehid,ownerid) select id, pVeh1,id from accounts;
+insert into carkeys(accountid,vehid,ownerid) select id, pVeh2,id from accounts;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
